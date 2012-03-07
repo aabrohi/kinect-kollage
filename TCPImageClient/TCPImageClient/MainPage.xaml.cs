07591f4a1f2572f726913f1d54bcd4a58aacd2f3
ddsniper <dDSniper@gmail.com>
2012-03-07 06:23:08 +0000

    This works! Uploading a picture from the phone creates a jpeg on the server in the Downloads folder.

---
+++ TCPImageClient/TCPImageClient/MainPage.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+using Microsoft.Phone.Tasks;
+using System.Text;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Net.Sockets;
+
+using System.Diagnostics;
+using System.Windows.Resources;
+
+
+namespace TCPImageClient
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        // Constants
+        const int IMAGE_PORT = 7070;  // Use port 7070, not assigned to a TCP/UDP Standard
+
+        String image2;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void button1_Click(object sender, RoutedEventArgs e)
+        {
+            image1.Source = null;
+            PhotoChooserTask objPhotoChooser = new PhotoChooserTask();
+            objPhotoChooser.Completed += new EventHandler<PhotoResult>(PhotoChooseCall);
+            objPhotoChooser.Show();
+        }
+
+        void PhotoChooseCall(object sender, PhotoResult e)
+        {
+            switch (e.TaskResult)
+            {
+                case TaskResult.OK:
+                    BinaryReader objReader = new BinaryReader(e.ChosenPhoto);
+                    image1.Source = new BitmapImage(new Uri(e.OriginalFileName));
+                    BitmapImage bi = new BitmapImage();
+                    bi.SetSource(e.ChosenPhoto);
+                    Uri uri = new Uri(e.OriginalFileName);
+                    image2 = e.OriginalFileName;
+                    Debug.WriteLine(image2);
+                    break;
+                case TaskResult.Cancel:
+                    MessageBox.Show("Cancelled");
+                    break;
+                case TaskResult.None:
+                    MessageBox.Show("Nothing Entered");
+                    break;
+            }
+        }
+
+        private void upload1_Click(object sender, RoutedEventArgs e)
+        {
+            // Make sure we can perform this action with valid data
+            if (ValidateRemoteHost())   //SET THIS TO TRUE IF YOU WANT TO BYPASS BLANK INPUT
+            {
+/*
+        //        Socket sListen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                IPAddress IP = IPAddress.Parse(txtRemoteHost.Text);
+                IPEndPoint IPE = new IPEndPoint(IP, 7070);
+
+        //        sListen.Bind(IPE); //WP 7.1 does not support this!
+
+                Console.WriteLine("Service is listening ...");
+        //        sListen.Listen(2); //WP 7.1 does not supprot this!
+
+                while (true)
+                {
+                    Socket clientSocket;
+                    try
+                    {
+                        clientSocket = sListen.Accept();
+                    }
+                    catch
+                    {
+                        throw;
+                    }
+
+                    // send data to the client
+                    //clientSocket.Send (Encoding.Unicode.GetBytes ("I am a server, you there?? !!!!"));
+
+                    // send the file
+                    byte[] buffer = ReadImageFile("1.jpg");
+                    clientSocket.Send(buffer, buffer.Length, SocketFlags.None);
+                    Console.WriteLine("Send success!");
+                }
+*/
+
+                // Instantiate the TCPSocket
+                SocketClient client = new SocketClient();
+
+                // Attempt to send our message to the server
+                string result = client.Connect(txtRemoteHost.Text, IMAGE_PORT);
+
+                // Receive a response from the server
+//                result = client.Send();
+
+                //string imageinstringformat = image2.Source;
+
+                //string location = @"\" + image2;
+                //Debug.WriteLine("Where is it? " + location);
+
+                //Debug.WriteLine(image2.ToString());
+
+                //byte[] buffer = ReadImageFile(image2);
+
+       //string message = "hello";
+
+                //ReadImageFile(image2);
+
+       //         result = client.Send(Encoding.UTF8.GetBytes(message));
+                string base64String = Convert.ToBase64String(ReadImageFile(image2));
+                //Debug.WriteLine("I'm sending this: " + base64String);
+                Debug.WriteLine("First part of the buffer: " + Encoding.UTF8.GetBytes(base64String + "<EOF>").Length);
+                result = client.Send(Encoding.UTF8.GetBytes(base64String));
+                Console.WriteLine("Send success!");
+
+                // Close the socket connection explicitly
+//                client.Close();
+            }
+        }
+
+        private static byte[] ReadImageFile(String img)
+        {
+
+            Uri jpegUri = new Uri(img, UriKind.Relative);
+            StreamResourceInfo sri = Application.GetResourceStream(jpegUri);
+            
+            byte[] buf = new byte[sri.Stream.Length];       //need to create a new buf everytime to account for varying image sizes
+                                                            //remedy: do not upload the same picture twice in a row/be a David
+                                                            //how to fix later... clear out the image1 from the screen everytime you click upload
+            sri.Stream.Read(buf, 0, buf.Length);
+          
+            return buf;
+        }
+
+
+        #region UI Validation
+        /// <summary>
+        /// Validates the txtRemoteHost TextBox
+        /// </summary>
+        /// <returns>True if the txtRemoteHost contains valid data, False otherwise</returns>
+        private bool ValidateRemoteHost()
+        {
+            // The txtRemoteHost must contain some text
+            if (String.IsNullOrWhiteSpace(txtRemoteHost.Text))
+            {
+                MessageBox.Show("Please enter a host name");
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ TCPSocketImageReceiverForm/TCPSocketImageReceiverForm/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+using System.Diagnostics; // for diagnosing, HOUSE MD style :P
+using System.Windows;
+using System.Threading;
+
+
+
+namespace TCPSocketImageReceiverForm
+{
+    // State object for reading client data asynchronously
+    public class StateObject
+    {
+        // Client  socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public const int BufferSize = 10485760; //10mb
+        // Receive buffer.
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+    }
+
+    public partial class Form1 : Form
+    {
+
+        //UdpClient udpc;
+        //IPEndPoint ep;
+        //Socket s;
+        //EndPoint Remote;
+        //IPEndPoint ipep;
+
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public Form1() //Initialize GUI
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(this.Form1_Load);
+            //Debug.WriteLine("Test");
+        }
+
+        // byte[] byteData = new byte[10485760]; // btw 10 485 760 = 10 mb
+
+
+
+        private void Form1_Load(object sender, EventArgs e) // Load after Form1
+        {
+            // Data buffer for incoming data.
+            //   byte[] bytes = new Byte[10485760];
+
+            // Establish the local endpoint for the socket.
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 7070);
+
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and listen for incoming connections.
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections.
+                    Console.WriteLine("Waiting for a connection...");
+                    listener.BeginAccept(
+                        new AsyncCallback(AcceptCallback),
+                        listener);
+
+                    // Wait until a connection is made before continuing.
+                    allDone.WaitOne();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.
+            allDone.Set();
+
+            // Get the socket that handles the client request.
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // Create the state object.
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReadCallback), state);
+
+
+
+            Debug.WriteLine("First part of the buffer: " + state.buffer[0]);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket
+            // from the asynchronous state object.
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            // Read data from the client socket. 
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.
+                state.sb.Append(Encoding.UTF8.GetString(
+                    state.buffer, 0, bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read 
+                // more data.
+                content = state.sb.ToString();
+
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    Debug.WriteLine("Content is: " + content);
+                    // All the data has been read from the  
+                    // client. Display it on the console.
+                    Console.WriteLine("Read {0} bytes from socket. \n ",
+                        content.Length);
+
+                    //                    Debug.WriteLine("For Brandon: " + modcontent);
+
+
+
+                    // Echo the data back to the client.
+                    //                 Send(handler, content);
+                }
+                else
+                {
+                    // Not all data received. Get more.
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+
+            byte[] imageBytes = Convert.FromBase64String(content);
+
+            FileStream fs = File.Create(@"C:\Users\ddsniper\Downloads\1.jpg");
+            fs.Write(imageBytes, 0, imageBytes.Length);
+            fs.Close();
+        }
+
+        /*        private void OnReceive(IAsyncResult ar)
+                {
+                    Debug.WriteLine("I got some stuff yo"); // Output this to the debug window if data is received
+
+                    FileStream fs = File.Create(@"C:\Users\ddsniper\Downloads\1.jpg");
+                    fs.Write(byteData, 0, byteData.Length);
+
+                    Image image = null;
+                    using (MemoryStream stream = new MemoryStream(data))
+                    {
+                        image = Image.FromStream(stream);
+                    }
+
+                    pictureBox1.Image = new Bitmap(image);
+                    pictureBox1.Show();
+
+                }
+        */
+    }
+}
